79fc6fba0d5574e97b53b8b44170f047c5145a8c
Filipe Jesse <filipejesse5@gmail.com>
2019-10-10 08:10:59 -0300

    Updates

---
+++ precache-manifest.c225cac22cf94d644fff34a2b683c9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3e45f9df156a324d0c3ca42baeb5bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "230b8da3ebe2fc8ef093",
+    "url": "/static/css/2.bceae682.chunk.css"
+  },
+  {
+    "revision": "ee30179b382e01787554",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "230b8da3ebe2fc8ef093",
+    "url": "/static/js/2.d35eb32c.chunk.js"
+  },
+  {
+    "revision": "ee30179b382e01787554",
+    "url": "/static/js/main.31f3d7ab.chunk.js"
+  },
+  {
+    "revision": "b70774a3bc4ed3b27e14",
+    "url": "/static/js/runtime-main.f1bad0ce.js"
+  }
+]);
